b86460de0144d91848374cc56f542dc7c45f378e
anishafishtoe <deliriousdandelion@gmail.com>
2016-05-16 21:36:27 +0530

    1. Correction of feeding station name 2. Plots of before and after

--- tree cutting exp.R
+++ tree cutting exp.R
@@ -2,29 +2,57 @@
 
 setwd("F:/NCBS/Thesis/Data/")
 
-expGuds=read.csv("Tree cutting exp.csv")
+expGuds=read.csv("Tree cutting exp2904.csv")
 
 str(expGuds)
 length(which(is.na(expGuds$GUDN)))
 length(which(is.na(expGuds$GUDF)))
 
+length(which(expGuds$GUDN>3))
+length(which(expGuds$GUDF>3))
+
 stripchart(expGuds$GUDN)
 stripchart(expGuds$GUDF)
 
+unique(expGuds$FStn)
+expGuds$FStn[which(expGuds$FStn=="TI")]="T1"
+
+treesUncut= unique(grep("nt+",expGuds$FStn , ignore.case=T, value=T, perl=T))
+treesCut=setdiff(unique(expGuds$FStn), treesCut)
+
+#Mean guds for cut vs un-cut trees.
+
+
 #mean guds
-gudN.mean=aggregate(expGuds$GUDN[-which(is.na(expGuds$GUDN))], by=list(BefOnAft=expGuds$BefOnAft[-which(is.na(expGuds$GUDN))],Date=expGuds$Date[-which(is.na(expGuds$GUDN))]), FUN=mean)
-colnames(gudN.mean)[3]="gudNMean"
-gudF.mean=aggregate(expGuds$GUDF[-which(is.na(expGuds$GUDF))], by=list(BefOnAft=expGuds$BefOnAft[-which(is.na(expGuds$GUDF))],Date=expGuds$Date[-which(is.na(expGuds$GUDF))]), FUN=mean)
-colnames(gudF.mean)[3]="gudFMean"
-nrow(gudF.mean)
 
-gudMeans=gudN.mean
-gudMeans$gudFMean=gudF.mean$gudFMean
+gudN.mean_c=aggregate(expGuds$GUDN[substring(expGuds$FStn,1,1)=="T"& !(is.na(expGuds$GUDN))], by=list(BefOnAft=expGuds$BefOnAft[substring(expGuds$FStn,1,1)=="T"& !(is.na(expGuds$GUDN))],Date=expGuds$Date[substring(expGuds$FStn,1,1)=="T"& !(is.na(expGuds$GUDN))]), FUN=mean)
+colnames(gudN.mean_c)[3]="gudMean"
+gudN.mean_c$nf=rep("N", nrow(gudN.mean_c))
+gudN.mean_c$cutUncut=rep("Cut", nrow(gudN.mean_c))
 
-gudMeans$BefOnAft=sapply(gudMeans$BefOnAft, function(x){if (x=="B") "Before" else if (x=="O") "On" else if (x=="A")"After"})
+gudN.mean_uc=aggregate(expGuds$GUDN[substring(expGuds$FStn,1,1)=="N"& !(is.na(expGuds$GUDN))], by=list(BefOnAft=expGuds$BefOnAft[substring(expGuds$FStn,1,1)=="N"& !(is.na(expGuds$GUDN))],Date=expGuds$Date[substring(expGuds$FStn,1,1)=="N"& !(is.na(expGuds$GUDN))]), FUN=mean)
+colnames(gudN.mean_uc)[3]="gudMean"
+gudN.mean_uc$nf=rep("N", nrow(gudN.mean_uc))
+gudN.mean_uc$cutUncut=rep("Uncut", nrow(gudN.mean_uc))
+
+
+gudF.mean_c=aggregate(expGuds$GUDF[substring(expGuds$FStn,1,1)=="T"& !(is.na(expGuds$GUDF))], by=list(BefOnAft=expGuds$BefOnAft[substring(expGuds$FStn,1,1)=="T"& !(is.na(expGuds$GUDF))],Date=expGuds$Date[substring(expGuds$FStn,1,1)=="T"& !(is.na(expGuds$GUDF))]), FUN=mean)
+colnames(gudF.mean_c)[3]="gudMean"
+gudF.mean_c$nf=rep("F", nrow(gudF.mean_c))
+gudF.mean_c$cutUncut=rep("Cut", nrow(gudN.mean_c))
+
+gudF.mean_uc=aggregate(expGuds$GUDF[substring(expGuds$FStn,1,1)=="N"& !(is.na(expGuds$GUDF))], by=list(BefOnAft=expGuds$BefOnAft[substring(expGuds$FStn,1,1)=="N"& !(is.na(expGuds$GUDF))],Date=expGuds$Date[substring(expGuds$FStn,1,1)=="N"& !(is.na(expGuds$GUDF))]), FUN=mean)
+colnames(gudF.mean_uc)[3]="gudMean"
+gudF.mean_uc$nf=rep("F", nrow(gudF.mean_uc))
+gudF.mean_uc$cutUncut=rep("Uncut", nrow(gudN.mean_uc))
+
+colnames(gudF.mean_c)
+gudMean=rbind(gudN.mean_c, gudN.mean_uc, gudF.mean_c, gudF.mean_uc)
+
+gudMean$BefOnAft=sapply(gudMean$BefOnAft, function(x){if (x=="B") "Before" else if (x=="O") "On" else if (x=="A")"After"})
 
 class(gudMeans$BefOnAft)
-gudMeans$BefOnAft= as.factor(gudMeans$BefOnAft)
+
 
 #Plot Guds
 
@@ -33,7 +61,26 @@ boxplot(gudMeans$gudFMean~gudMeans$BefOnAft, ylab="GUD-Open")
 
 par(mfrow=c(1,2))
 
-#Mean guds for cut vs un-cut trees.
-nRows=length(unique(expGuds$Date))
-gMeans_c_uc= data.frame(BefOnAft=vector(length=nRows), gudM_n_c=vector(length=nRows),gudM__n_uc=vector(length=nRows), gudM_f_c= vector(length=nRows), gudM_f_uc=vector(length=nRows)  )
 
+
+
+#plots
+
+gudMean$BefOnAft=factor(gMeans$BefOnAft, c("Before","On","After"))
+
+
+par(mfrow=c(2,2))
+boxplot(gMeans$gudM_n_c~gMeans$BefOnAft, ylab="GUD", main="Bush- Cut")
+boxplot(gMeans$gudM__n_uc~gMeans$BefOnAft, ylab="GUD", main="Bush- Uncut")
+boxplot(gMeans$gudM_f_c~gMeans$BefOnAft, ylab="GUD", main="Open- Cut")
+boxplot(gMeans$gudM_f_uc~gMeans$BefOnAft, ylab="GUD", main="Open- Uncut")
+
+
+
+#30-04-2015
+
+#Plot using dataframe containing separate entries for cut, uncut trees and near and fear stations
+par(mfrow=c(2,2))
+for (i in c("Before","On","After")){
+  boxplot(gudMean$gudMean~gudMean$nf+gudMean$cutUncut, ylab="GUD", main=i)
+}
