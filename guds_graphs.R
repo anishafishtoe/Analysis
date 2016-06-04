90ae5016edd834b77a712ab009df2ed58757bcff
anishafishtoe <deliriousdandelion@gmail.com>
2016-06-04 19:54:17 +0530

    1. Histogram of GUDs for each habitat 2. PLot activity across night

--- guds_graphs.R
+++ guds_graphs.R
@@ -9,7 +9,13 @@ hist(log(gMean$meanGUD), xlab="Mean GUD Transformed", main="")
 hist(sqrt(gMean$meanGUD))
 hist((gMean$meanGUD)^2)
 
+#How are GUDs distributed for each habitat
+with(guds, hist(GUD[Habitat=="Sparse"],xlab="GUD", main="", col="#A4A4A4"))
+with(guds, hist(GUD[Habitat=="Dense"], col="#04B4AE", add=T))
+legend("topleft", c("Sparse", "Dense"), col=c("#A4A4A4", "#04B4AE") ,pch=16)
+
 #How much did ants and birds eat up the seeds
+
 
 guds.ants=c(guds$GUDN[which(guds$AntBirdN=="A" & is.na(guds$PrintsInside))],guds$GUDF[which(guds$AntBirdF=="A" & is.na(guds$PrintsInside))] )
 boxplot(guds.ants, xlab="Ants", ylab="Number of seeds harvested")
@@ -40,7 +46,7 @@ plot(gud.lm.int)
 
 #effect of moon phase
 
-gud.mPhase=with(gMean, describeBy(sqrt(meanGUD), moonPhase, mat=T, digits=4))
+gud.mPhase=with(gMean, describeBy(meanGUD, moonPhase, mat=T, digits=4))
 names(gud.mPhase)[names(gud.mPhase)=="group1"]="MoonPhase"
 limits=with(gud.mPhase,aes(ymax=mean+(1.96*se), ymin=mean-(1.96*se)))
 dodge=position_dodge(width=0.9)
@@ -48,11 +54,12 @@ ggplot(gud.mPhase, aes(x=MoonPhase, y=mean))+
   geom_point(stat='identity', position=dodge)+
   geom_errorbar(limits, position=dodge, width=0.25)+
   ylab('Mean GUD')+
+  theme_set(theme_gray(base_size = 20))+
   scale_fill_grey()
 
 #effect of habitat
 
-gud.hab=with(gMean, describeBy(sqrt(meanGUD), habitat, mat=T, digits=4))
+gud.hab=with(gMean, describeBy(meanGUD, habitat, mat=T, digits=4))
 names(gud.hab)[names(gud.hab)=="group1"]="Habitat"
 limits=with(gud.hab,aes(ymax=mean+(1.96*se), ymin=mean-(1.96*se)))
 dodge=position_dodge(width=0.9)
@@ -60,11 +67,14 @@ ggplot(gud.hab, aes(x=Habitat, y=mean))+
   geom_point(stat='identity', position=dodge)+
   geom_errorbar(limits, position=dodge, width=0.25)+
   ylab('Mean GUD')+
+  theme_set(theme_gray(base_size = 20))+
   scale_fill_grey()
+
+
 
 #effect of microhabitat
 
-gud.mhab=with(gMean, describeBy(sqrt(meanGUD),feedingTrayPosition , mat=T, digits=4))
+gud.mhab=with(gMean, describeBy(meanGUD,feedingTrayPosition , mat=T, digits=4))
 names(gud.mhab)[names(gud.mhab)=="group1"]="FeedingTrayPosition"
 limits=with(gud.mhab,aes(ymax=mean+(1.96*se), ymin=mean-(1.96*se)))
 dodge=position_dodge(width=0.9)
@@ -72,25 +82,26 @@ ggplot(gud.mhab, aes(x=FeedingTrayPosition, y=mean))+
   geom_point(stat='identity', position=dodge)+
   geom_errorbar(limits, position=dodge, width=0.25)+
   ylab('Mean GUD')+
+  theme_set(theme_gray(base_size = 20))+
   scale_fill_grey()
 
 #effect of month
 
-gud.month=with(gMean, describeBy(sqrt(meanGUD),month , mat=T, digits=4))
+gud.month=with(gMean, describeBy(meanGUD,month , mat=T, digits=4))
 names(gud.month)[names(gud.month)=="group1"]="Month"
 limits=with(gud.month,aes(ymax=mean+(1.96*se), ymin=mean-(1.96*se)))
 dodge=position_dodge(width=0.9)
 ggplot(gud.month, aes(x=Month, y=mean))+ 
   geom_point(stat='identity', position=dodge)+
   geom_errorbar(limits, position=dodge, width=0.25)+
+  theme_set(theme_gray(base_size = 20))+
   ylab('Mean GUD')+
   scale_fill_grey()
 
 
 #plot interaction between moon phase and feeding tray position
 
-par(mfrow=c(1,1))
-mPhase.trayPos.summary=with(gMean,describeBy(sqrt(meanGUD), list(moonPhase, feedingTrayPosition), mat=T, digits=2))
+mPhase.trayPos.summary=with(gMean,describeBy(meanGUD, list(moonPhase, FeedingTrayPosition), mat=T, digits=2))
 names(mPhase.trayPos.summary)[names(mPhase.trayPos.summary)=="group1"]="MoonPhase"
 names(mPhase.trayPos.summary)[names(mPhase.trayPos.summary)=="group2"]="FeedingTrayPosition"
 limits=with(mPhase.trayPos.summary,aes(ymax=mean+(1.96*se), ymin=mean-(1.96*se)))
@@ -104,7 +115,7 @@ ggplot(mPhase.trayPos.summary, aes(x=MoonPhase, y=mean, fill=FeedingTrayPosition
 
 #plot interaction between moon phase and habitat
 
-mPhase.hab.summary=with(gMean,describeBy(sqrt(meanGUD), list(moonPhase, habitat), mat=T, digits=4))
+mPhase.hab.summary=with(gMean,describeBy(meanGUD, list(moonPhase, habitat), mat=T, digits=4))
 names(mPhase.hab.summary)[names(mPhase.hab.summary)=="group1"]="MoonPhase"
 names(mPhase.hab.summary)[names(mPhase.hab.summary)=="group2"]="Habitat"
 limits=with(mPhase.hab.summary,aes(ymax=mean+(1.96*se), ymin=mean-(1.96*se)))
@@ -116,9 +127,19 @@ ggplot(mPhase.hab.summary, aes(x=MoonPhase, y=mean, fill=Habitat))+
   theme_set(theme_gray(base_size = 20))+
   scale_fill_grey()
 
+#plot activity across night
+ggplot(data=activity, aes(x=MoonPhase, y=Ncrossing, fill=Night))+geom_boxplot()+
+  theme_set(theme_gray(base_size = 20))+scale_fill_brewer()
+
+#ggplot activity across night for each month
+ggplot(data=activity, aes(x=MoonPhase, y=Ncrossing, fill=Night))+geom_boxplot()+
+  facet_wrap(~month)+
+  theme_set(theme_gray(base_size = 20))+scale_fill_brewer()
+
+
 #interaction between habitat and microhabitat
 
-hab.micro.summary=with(gMean,describeBy(sqrt(meanGUD), list(habitat, feedingTrayPosition), mat=T, digits=4))
+hab.micro.summary=with(gMean,describeBy(meanGUD, list(habitat, FeedingTrayPosition), mat=T, digits=4))
 names(hab.micro.summary)[names(hab.micro.summary)=="group1"]="Habitat"
 names(hab.micro.summary)[names(hab.micro.summary)=="group2"]="FeedingTrayPosition"
 limits=with(hab.micro.summary,aes(ymax=mean+(1.96*se), ymin=mean-(1.96*se)))
@@ -146,7 +167,7 @@ ggplot(mPhase.mon.summary, aes(x=Month, y=mean, fill=MoonPhase))+
 
 #interaction between habitat and month
 
-hab.mon.summary=with(gMean,describeBy(sqrt(meanGUD), list(habitat, month), mat=T, digits=4))
+hab.mon.summary=with(gMean,describeBy(meanGUD, list(habitat, month), mat=T, digits=4))
 names(hab.mon.summary)[names(hab.mon.summary)=="group1"]="Habitat"
 names(hab.mon.summary)[names(hab.mon.summary)=="group2"]="Month"
 limits=with(hab.mon.summary,aes(ymax=mean+(1.96*se), ymin=mean-(1.96*se)))
@@ -173,7 +194,7 @@ ggplot(micro.mon.summary, aes(x=Month, y=mean, fill=FeedingTrayPosition))+
   scale_fill_grey()
 
 #interaction between habitat, microhabitat and month
-mon.hab.muhab.summary=with(gMean,describeBy(sqrt(meanGUD), list(month, habitat, feedingTrayPosition), mat=T, digits=4))
+mon.hab.muhab.summary=with(gMean,describeBy(meanGUD, list(month, habitat, feedingTrayPosition), mat=T, digits=4))
 names(mon.hab.muhab.summary)[names(mon.hab.muhab.summary)=="group1"]="Month"
 names(mon.hab.muhab.summary)[names(mon.hab.muhab.summary)=="group2"]="Habitat"
 names(mon.hab.muhab.summary)[names(mon.hab.muhab.summary)=="group3"]="FeedingTrayPosition"
@@ -190,7 +211,7 @@ ggplot(data=mon.hab.muhab.summary, aes(x=Habitat, y=mean, fill=FeedingTrayPositi
   scale_fill_grey()
 
 #plot interaction between moon phase, habitat and microhabitat
-mPhase.hab.muhab.summary=with(gMean,describeBy(sqrt(meanGUD), list(moonPhase, habitat, feedingTrayPosition), mat=T, digits=4))
+mPhase.hab.muhab.summary=with(gMean,describeBy(meanGUD, list(moonPhase, habitat, feedingTrayPosition), mat=T, digits=4))
 names(mPhase.hab.muhab.summary)[names(mPhase.hab.muhab.summary)=="group1"]="MoonPhase"
 names(mPhase.hab.muhab.summary)[names(mPhase.hab.muhab.summary)=="group2"]="Habitat"
 names(mPhase.hab.muhab.summary)[names(mPhase.hab.muhab.summary)=="group3"]="FeedingTrayPosition"
