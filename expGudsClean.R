fc09089d6cc40edd620d4285ff2133e807457f56
anishafishtoe <deliriousdandelion@gmail.com>
2016-06-14 10:46:12 +0530

    1. Correct FStn name 2. Separate column for cut and uncut trees 3. Rearrange factor levels 4. Omit first day before cutting 5. Omit data on the day tree was cut

--- expGudsClean.R
+++ expGudsClean.R
@@ -10,23 +10,33 @@ library(plyr)
 colnames(expGuds)[which(colnames(expGuds)=="FStn")]="fStn"
 
 #Correct feeding station name
-expGuds$FStn[which(expGuds$FStn=="TI")]="T1"
+expGuds$fStn[which(expGuds$fStn=="TI")]="T1"
 
 #Set destroyed trays to NA
-expGuds$GUDF[expGuds$Date=="15-03-2016" & expGuds$FStn=="T3"]=NA
+expGuds$GUDF[expGuds$Date=="15-03-2016" & expGuds$fStn=="T3"]=NA
 
 #Separate column for cut and uncut trees
-expGuds$cutUncut= ifelse(substring(expGuds$FStn,1,1)=="T", "Cut", "Uncut")
+expGuds$cutUncut= ifelse(substring(expGuds$fStn,1,1)=="T", "Cut", "Uncut")
 
 #Delete columns
 expGuds=expGuds[-which(colnames(expGuds) %in% c("PrintsAround","PrintsInside","Digging","Destroyed","Comments"))]
 
 #Re-arrange factor levels of time of cutting
-gudMean$BefOnAft=factor(gMeans$BefOnAft, c("Before","On","After"))
+expGuds$BefOnAft=revalue(expGuds$BefOnAft, c("B"= "Before", "A"="After", "O"="On"))
+expGuds$BefOnAft=factor(expGuds$BefOnAft, c("Before","On","After"))
 
 #convert GUDS from wide to long
 expGuds=gather(expGuds, NF, GUD, GUDN, GUDF)
 expGuds$NF= ifelse(expGuds$NF=="GUDN","N","F")
+
+#Omit first day before cutting
+expGuds= expGuds[-which(expGuds$Date %in% "15-03-2016"),]
+expGuds$Date= factor(expGuds$Date)
+
+#Omit data on the day tree was cut
+expGuds= expGuds[-which(expGuds$BefOnAft %in% "On"),]
+expGuds$BefOnAft=factor(expGuds$BefOnAft)
+expGuds$Date= factor(expGuds$Date)
 
 #write into file
 write.csv(expGuds,"expGudsClean.csv")
