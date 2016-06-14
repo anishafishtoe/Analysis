fc0fab460f5d2ac767217f3a00e44531b0e7747b
anishafishtoe <deliriousdandelion@gmail.com>
2016-06-14 10:45:04 +0530

    1. separate models for cut and uncut (old) 2. which distribution fits the data 3. Compare AICs of different models

--- expGudsModels.R
+++ expGudsModels.R
@@ -2,16 +2,60 @@
 
 library(lme4)
 library(car)
+library(multcomp)
 
-full.model=lmer(data=expGuds, GUD~BefOnAft+NF+cutUncut+(1|fStn), REML=FALSE)
-Anova(full.model)
+full.model.cut=lmer(data=subset(expGuds, cutUncut=="Cut"), GUD~BefOnAft+NF+BefOnAft*NF+(1|fStn), REML=FALSE)
+Anova(full.model.cut)
+full.model.uncut=lmer(data=subset(expGuds, cutUncut=="Uncut"), GUD~BefOnAft+NF+BefOnAft*NF+(1|fStn), REML=FALSE)
+Anova(full.model.uncut)
+
+#Separate models for cut and uncut trees
+full.model.cut=lmer(data=subset(expGuds, cutUncut=="Cut"), GUD~BefOnAft+NF+(1|fStn), REML=FALSE)
+Anova(full.model.cut)
+full.model.uncut=lmer(data=subset(expGuds, cutUncut=="Uncut"), GUD~BefOnAft+NF+(1|fStn), REML=FALSE)
+Anova(full.model.uncut)
+
+#Compare activity with time of tree cutting
+summary(glht(full.model.cut, mcp(BefOnAft="Tukey")))
+summary(glht(full.model.uncut, mcp(BefOnAft="Tukey")))
+
 
 dayofCutting=lmer(data=expGuds, GUD~BefOnAft +(1|fStn), REML=FALSE)
 summary(dayofCutting)
 Anova(dayofCutting)
 
+#--13-06---
 
-a=aov(data=expGuds, GUD~BefOnAft)
-TukeyHSD(a)
+#How is the response variable distributed?
+hist(expGuds$GUD)
 
-library(lmerTest)
+#Which distribution fits the data?
+qqp(expGuds$GUD, "norm")
+qqp(expGuds$GUD, "lnorm")
+nbinom=fitdistr(expGuds$GUD,"Negative Binomial")
+qqp(expGuds$GUD,"nbinom", size= nbinom$estimate[[1]], mu=nbinom$estimate[[2]])
+poisson=fitdistr(expGuds$GUD, "Poisson")
+qqp(expGuds$GUD, "pois", poisson$estimate)
+gamma=fitdistr(expGuds$GUD, "gamma")
+qqp(expGuds$GUD, "gamma", shape=gamma$estimate[[1]], rate=gamma$estimate[[2]])
+
+#lmm
+gud.full=lmer(data=expGuds, GUD~BefOnAft+NF+BefOnAft+cutUncut+BefOnAft*cutUncut*NF+(1|fStn), REML=FALSE)
+Anova(gud.full)
+gud.BefOnAft=lmer(data=expGuds, GUD~BefOnAft+(1|fStn), REML=FALSE)
+gud.cutUncut=lmer(data=expGuds, GUD~cutUncut+(1|fStn), REML=FALSE)
+gud.TimeCut=lmer(data=expGuds, GUD~BefOnAft*cutUncut+(1|fStn), REML=FALSE)
+gud.cutNF=lmer(data=expGuds, GUD~cutUncut*NF+(1|fStn), REML=FALSE)
+gud.3way=lmer(data=expGuds, GUD~cutUncut*NF+BefOnAft*NF*cutUncut+(1|fStn), REML=FALSE)
+anova(gud.BefOnAft, gud.cutUncut, gud.TimeCut, gud.cutNF, gud.3way)
+
+#post hoc test
+require(lsmeans)
+  #interaction between time of cutting and cut vs uncut
+  lsm=lsmeans(gud.TimeCut, ~BefOnAft*cutUncut)
+  summary(lsm, type="response")
+
+  #3way
+  lsm.3way=lsmeans(gud.3way, ~BefOnAft*cutUncut*NF)
+  summary(lsm.3way, type="response")
+  
